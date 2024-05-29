5656f6df1dcdf117aa4ead6f28c4d5886172cd7f
MewaKitty <mewakitty@gmail.com>
2024-05-28 21:21:23 -0700

    Remove pink and gray

--- common/cards/card.ts
+++ common/cards/card.ts
@@ -27,7 +27,7 @@ export type GameConstants = {
 };
 
 export const defaultConstants: GameConstants = {
-	colors: ["red", "blue", "yellow", "green", "orange", "purple", "pink", "gray"],
+	colors: ["red", "blue", "yellow", "green", "orange", "purple"],
 	wilds: [],
 	includeMulticolorWild: true,
 	numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
